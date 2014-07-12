2e97e1f698b55788f5dc22c9007bc1c28e9ff41b
mpr0xy <mpr0xy@live.com>
2014-07-12 23:35:33 +0800

    add fabric

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import sudo, task, env, hosts
+from fabric.context_managers import cd
+
+env.use_ssh_config = True
+
+
+@task(default=True)
+@hosts('my-blog')
+def run_deploy(branch='master'):
+  deploy(branch)
+
+
+def deploy(branch):
+  with cd('/data/apps/my-blog'):
+    sudo('git fetch')
+    sudo('git checkout ' + branch)
+    sudo('git reset --hard origin/' + branch)
+    sudo('supervisorctl update')
+    sudo('supervisorctl restart my-blog')
+
+
+
+
